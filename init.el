3017d319bcbf22b8ee4da217f2467901c0b10645
Pratik Abhyankar <abhyankarpratik@gmail.com>
2022-05-31 16:37:25 -0700

    Add more core config and update gitignore

--- init.el
+++ init.el
@@ -12,22 +12,25 @@
 (require 'org)
 (require 'ob-tangle)
 
-; By default, org-babel :tangle is set to "no". Change it to "yes" so that
-; all literate config config files are tangled and loaded by 'org-babel'.
+					; By default, org-babel :tangle is set to "no". Change it to "yes" so that
+					; all literate config config files are tangled and loaded by 'org-babel'.
 (setq org-babel-default-header-args
       (cons '(:tangle . "yes")
             (assq-delete-all :tangle org-babel-default-header-args)))
 
 (defun pratik/load-config-file (file)
   "Load a literate org config file from the 'config' directory"
-  (defconst FILE-PATH (expand-file-name (concat user-emacs-directory "config/" file)))
-  (org-babel-load-file FILE-PATH))
+  (defconst ORG-CONFIG-FILE (expand-file-name (concat user-emacs-directory "config/" file ".org")))
+  (defconst EL-CONFIG-FILE (expand-file-name (concat user-emacs-directory "config/" file ".el")))
+  (org-babel-tangle-file ORG-CONFIG-FILE)
+  (if (file-exists-p EL-CONFIG-FILE)
+    (org-babel-load-file ORG-CONFIG-FILE)))
 
-(pratik/load-config-file "constants.org")
-(pratik/load-config-file "bootstrap.org")
-(pratik/load-config-file "appearance.org")
-(pratik/load-config-file "core.org")
-(pratik/load-config-file "org.org")
-(pratik/load-config-file "dev.org")
+(pratik/load-config-file "constants")
+(pratik/load-config-file "bootstrap")
+(pratik/load-config-file "appearance")
+(pratik/load-config-file "core")
+(pratik/load-config-file "org")
+(pratik/load-config-file "dev")
 
 ;;; init.el ends here
