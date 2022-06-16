b4af8870a526a502851910b33af48b8dc5b78b03
Pratik Abhyankar <abhyankarpratik@gmail.com>
2022-06-15 18:40:23 -0700

    Add vterm, org-superstar and modes config for python and ledger

---
+++ config/modes/ledger.el
@@ -0,0 +1,1 @@
+(use-package ledger-mode)
--- init.el
+++ init.el
@@ -18,13 +18,25 @@
       (cons '(:tangle . "yes")
             (assq-delete-all :tangle org-babel-default-header-args)))
 
+
+(defconst CONFIG-DIR (expand-file-name "config/" user-emacs-directory))
+
 (defun pratik/load-config-file (file)
   "Load a literate org config FILE from the 'config' directory."
-  (defconst ORG-CONFIG-FILE (expand-file-name (concat user-emacs-directory "config/" file ".org")))
-  (defconst EL-CONFIG-FILE (expand-file-name (concat user-emacs-directory "config/" file ".el")))
+  (defconst ORG-CONFIG-FILE (expand-file-name (concat CONFIG-DIR  file ".org")))
+  (defconst EL-CONFIG-FILE (expand-file-name (concat CONFIG-DIR file ".el")))
   (org-babel-tangle-file ORG-CONFIG-FILE)
   (if (file-exists-p EL-CONFIG-FILE)
     (org-babel-load-file ORG-CONFIG-FILE)))
+
+(defun pratik/load-mode-config-file (file)
+  "Load a literate org mode config FILE from the 'config/modes/' directory."
+  (defconst MODES-DIR (expand-file-name "modes/" CONFIG-DIR))
+	(defconst ORG-MODE-CONFIG-FILE (expand-file-name (concat MODES-DIR  file ".org")))
+	(defconst EL-MODE-CONFIG-FILE (expand-file-name (concat MODES-DIR file ".el")))
+	(org-babel-tangle-file ORG-MODE-CONFIG-FILE)
+	(if (file-exists-p EL-MODE-CONFIG-FILE)
+			(org-babel-load-file ORG-MODE-CONFIG-FILE)))
 
 (pratik/load-config-file "constants")
 (pratik/load-config-file "bootstrap")
@@ -33,5 +45,8 @@
 (pratik/load-config-file "org")
 (pratik/load-config-file "dev")
 (pratik/load-config-file "eloquence")
+
+(pratik/load-mode-config-file "ledger")
+(pratik/load-mode-config-file "python")
 
 ;;; init.el ends here
