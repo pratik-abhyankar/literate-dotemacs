25ed5560aa00ddc0d50bba24d9b2000c1c9ff53d
Pratik Abhyankar <abhyankarpratik@gmail.com>
2022-06-30 00:25:09 -0700

    Modify gitignore to ignore el files from modes dir

--- config/modes/ledger.el
+++
@@ -1,9 +0,0 @@
-(unless (executable-find "ledger")
-  (when IS-LINUX
-    (async-shell-command "sudo -S apt install ledger"))
-  (when IS-MAC
-    (async-shell-command "brew install ledger")))
-(use-package ledger-mode)
-
-(use-package flycheck-ledger
-  :after flycheck ledger-mode)
